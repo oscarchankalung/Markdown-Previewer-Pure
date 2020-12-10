11fddd0cbc718621cdcebf8619082b975bd83ce1
Oscar Chan <oscarchankalung@gmail.com>
2020-12-10 21:50:14 +0800

    add placeholder that serves as default markdown

--- model/placeholder.js
+++ model/placeholder.js
@@ -39,6 +39,7 @@ And here. | Okay. | I think we get it.
 1. And there are numbererd lists too.
 1. Use just 1s if you want!
 1. And last but not least, let's not forget embedded images:
-`;
+
+![React Logo w/ Text](https://goo.gl/Umyytc)`;
 
 export default placeholder;

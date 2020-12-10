920f742286cfc4094643fe7fe94b56b493a984c9
Oscar Chan <oscarchankalung@gmail.com>
2020-12-10 21:49:48 +0800

    add Previewer that render Markdown into HTML using GitHub Markdown API

--- model/Previewer.js
+++ model/Previewer.js
@@ -2,8 +2,22 @@ function Previewer () {
   this.view = document.querySelector('#preview');
 }
 
-Previewer.prototype.setHTML = function (markdown) {
-  this.view.innerHTML = marked(markdown);
+Previewer.prototype.render = async function (markdown) {
+  const href = 'https://api.github.com/markdown';
+  const body = JSON.stringify({ text: markdown });
+
+  const response = await fetch(href, {
+    method: 'POST',
+    body: body,
+    headers: { Accept: 'application/vnd.github.v3+json' }
+  });
+
+  if (!response.ok) {
+    throw new Error(response.message);
+  } else {
+    const text = await response.text();
+    this.view.innerHTML = text;
+  };
 };
 
 export default Previewer;
